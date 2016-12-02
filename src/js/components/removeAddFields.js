68786bde308d7dc8d74d93418765d8b6cae0d02d
adulfan <Anna.Dulfan@ey.com>
2016-12-02 16:33:53 -0500

    started to work on the add remove functionality

--- src/js/components/removeAddFields.js
+++ src/js/components/removeAddFields.js
@@ -5,17 +5,42 @@
 */
 var addRemoveFields = (function ($) {
   'use strict';
+  var cloned_row = $('.main-input').clone(true).html();
 
   function initaddRemoveFields(form) {
-    $('.fa-trash').on('click',function () {
-      $(this).parent().prevAll().remove();
-      $(this).parent().remove();
-    });
-    $('.add-row').on('click', function() {
-      var row = $(this).parent().parent().prev().clone();
-      $(this).parent().parent().prev().before(row).find( 'input:text' ).val( '' );
+    // var html_row = '<div class="row\">' +
+    //     '<div class="col-11">'  +
+    //       '<div class="field-wrapper">'+
+    //       '<label for="city">Deliverables/Work Stream</label>' +
+    //       '<input name="city" placeholder="Deliverables/Work Stream" value="" />' +
+    //       '</div></div>' +
+    //       '<div class="col-1">' +
+    //       '<button type="button" class="btn btn-blue fa fa-trash" value=""></button>' +
+    //       '</div></div>';
+
+
+    $('.main-input').on('click', function(e) {
+      var target = $(e.target);
+      var $this = $(this);
+      var remove_row = $this;
+
+      if(target.hasClass('fa-trash')) {
+        remove_row.remove();
+      }
+      if(target.hasClass('add-row')) {
+        var row = $(this).prev().clone(true);
+        console.log(cloned_row);
+        $(this).prepend(cloned_row);
+        // remove_row.prev().before(row).find( 'input' ).val( '' );
+        // if(row.length > 0) {
+        //   remove_row.prev().before(row).find( 'input' ).val( '' );
+        // }  else {
+        //   remove_row.before(html_row);
+        // }
+      }
     });
   }
+
   return {
     initaddRemoveFields:initaddRemoveFields
   }
