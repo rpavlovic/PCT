4529ba60875e0a3611c995c56a4ef16864b37fac
adulfan <Anna.Dulfan@ey.com>
2016-12-06 17:25:56 -0500

    fixed js to look better

--- src/js/components/progressNav.js
+++ src/js/components/progressNav.js
@@ -7,28 +7,24 @@ var progressNav = (function ($) {
 
   function initProgressNav(form) {
     var radio_href_value = $(form + ' input[type=radio]').val(),
+        current_li = $(form + ' li.current'),
         radio = $(form + ' input[type=radio]');
 
     //TODO make pagination like to click back to the previously filled form.
     if((radio_href_value !== window.location.pathname) || ($(form + ' li:not("current")'))) {
-      $(form + ' input[type=radio]').prop('disabled', true);
-      $(form + ' li.current').prev().children('input').prop('disabled', false);
-      $(form + ' li.current').prevAll().addClass('previous');
+      radio.prop('disabled', true);
+      current_li.prev().children('input').prop('disabled', false);
+      current_li.prevAll().addClass('previous');
     }
-
-    $(form + ' li.current').children('input[type=radio]').prop('checked', true);
-    console.log($(form + ' li.current').prev().children('input'));
-    for(var i = 0; i < radio.length; i++) {
-      $(form + ' input[type=radio]').on('click', function(event) {
+    current_li.children(radio).prop('checked', true);
+      radio.on('click', function(event) {
         event.preventDefault();
         var href = $(form + ' input[type=radio]:checked').val();
         window.location = href;
       });
     }
-
-  }
-  return {
-    initProgressNav:initProgressNav
+    return {
+      initProgressNav:initProgressNav
   }
 
 })($);
