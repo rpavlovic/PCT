0c040de66cbe0b0ad1e0d04b5e8f10a156d1d550
adulfan <Anna.Dulfan@ey.com>
2016-12-20 16:49:53 -0500

    added to date fields monthly logic

--- src/js/components/projectDateFields.js
+++ src/js/components/projectDateFields.js
@@ -18,35 +18,36 @@ var projectDuration = (function ($) {
         dateFormat = "mm/dd/yyyy";
 
         if(plan_by === "Weekly") {
-          daysToAdd = 7;
+          daysToAdd = 7 * parseInt(duration);
         } else if (plan_by === "Monthly") {
-          daysToAdd = daysInMonth(selected_month, inst.selectedYear);
+          daysToAdd = daysInMonth(selected_month * parseInt(duration), inst.selectedYear);
         }
 
 
-        var curr = new Date($(form + " .datepicker").val()), // get current date
-                   first = curr.getDate(),
-                   daysToAdd,
-                   last = (first + daysToAdd) * duration,
-                   lastday = new Date(curr.setDate(last)),
-                   month = lastday.getMonth() + 1,
+        var curr = new Date(estimated_start_date), // get current date
+          first = curr.getDate(),
+          daysToAdd,
+          last = first + daysToAdd,
+          lastday = new Date(curr.setDate(last)),
+          month = (lastday.getMonth() + 1) * parseInt(duration),
 
-                   weekly_month = (month*duration) < 10 ? '0' + month : '' + month;
+          weekly_month = (month) < 10 ? '0' + month : '' + month;
 
         function daysInMonth(month,year) {
           return new Date(year, month, 0).getDate();
         }
 
-
     function choose_planBy() {
+      console.log(weekly_month);
       $(form + " select[name='planby'] option:selected").each(function() {
         if(inst.input.val != '' && $.isNumeric(duration) || $.isNumeric(duration.length > 0)) {
           switch($(this).val()) {
             case 'Weekly':
-                estimate_end_date.val(weekly_month + '/' + lastday.getDate() + '/' + lastday.getFullYear());
-                $(form + " input[name=\"duration\"]").val(duration + (duration == 1 ? ' Week': ' Weeks'));
+              estimate_end_date.val(weekly_month + '/' + lastday.getDate() + '/' + lastday.getFullYear());
+              $(form + " input[name=\"duration\"]").val(duration + (duration == 1 ? ' Week': ' Weeks'));
               break;
             case 'Monthly':
+
               estimate_end_date.val(weekly_month + '/' + lastday.getDate() + '/' + lastday.getFullYear());
               $(form + " input[name=\"duration\"]").val(duration + (duration == 1 ? ' Month': ' Months'));
               break;
