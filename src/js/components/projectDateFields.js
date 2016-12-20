111e2a56965bc8059c4f7f412f738aaa30ffdd7b
adulfan <Anna.Dulfan@ey.com>
2016-12-20 16:24:48 -0500

    added Date mnipulation

--- src/js/components/projectDateFields.js
+++ src/js/components/projectDateFields.js
@@ -27,17 +27,11 @@ var projectDuration = (function ($) {
         var curr = new Date($(form + " .datepicker").val()), // get current date
                    first = curr.getDate(),
                    daysToAdd,
-                   last = first + daysToAdd,
+                   last = (first + daysToAdd) * duration,
                    lastday = new Date(curr.setDate(last)),
                    month = lastday.getMonth() + 1,
 
-                   weekly_month = month < 10 ? '0' + month : '' + month;
-
-
-
-
-
-console.log(estimate_end_date.val(weekly_month + '/' + lastday.getDate() + '/' + lastday.getFullYear()));
+                   weekly_month = (month*duration) < 10 ? '0' + month : '' + month;
 
         function daysInMonth(month,year) {
           return new Date(year, month, 0).getDate();
