6bfb46a84eb8292435a1f5ac44fe785b02a5d2bd
adulfan <Anna.Dulfan@ey.com>
2016-12-20 14:24:09 -0500

    Issue #29 completed

--- src/js/components/loadJSON.js
+++ src/js/components/loadJSON.js
@@ -6,11 +6,12 @@
 var loadJSON = (function ($) {
   'use strict';
 
-  var items = [];
+  var items = [],
+      select = $("form.project-info select[name='billing']");
 
-  function Iterate_billing_office_JSON(data1, data2) {
-    console.log(data2);
-    var select = $("form.project-info select[name='billing']");
+
+  //prepopulate Billing office select with JSON data.
+  function prepopulate_Billing_Office_JSON(data1) {
     $.each(data1.d.results, function(key, val) {
       for (var key in val) {
         if(key === "OfficeName") {
@@ -21,11 +22,30 @@ var loadJSON = (function ($) {
     select.append(jQuery.unique(items));
   }
 
+  //match the employee office with list of offices and select the matching one.
+  function prepopulate_Employee_Office(data2) {
+    $.each(data2.d.results, function(key, val) {
+      for (var key in val) {
+        if(key === "OfficeName") {
+          $.each( select[0].options, function() {;
+            if( $(this).val() === val[key]) {
+              $(this).prop('selected', true);
+            }
+          });
+        }
+      }
+    });
+  }
+
   function initJSON(jsonFile1, jsonFile2) {
-    $.getJSON(jsonFile1, jsonFile2,  function(data1, data2) {
-      Iterate_billing_office_JSON(data1, data2);
+
+    $.getJSON(jsonFile1,  function(data1) {
+      prepopulate_Billing_Office_JSON(data1);
     })
     .done(function() {
+      $.getJSON(jsonFile2,  function(data2) {
+         prepopulate_Employee_Office(data2);
+      });
       floatLabel.initfloatLabel();
     })
     .fail(function(jqXHR, exception) {
