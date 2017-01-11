9122456764e2832af730e1bdf80b9bd4a42867b5
adulfan <Anna.Dulfan@ey.com>
2017-01-11 13:36:40 -0500

    update cvs

--- src/js/components/captureTableEditFileds.js
+++ src/js/components/captureTableEditFileds.js
@@ -10,6 +10,7 @@ var captureEditTd = (function ($) {
     $(table).on('keydown', function (event) {
       var esc = event.which == 27,
           nl = event.which == 13,
+          tab = event.which == 9,
           el = event.target,
           input = el.nodeName != 'INPUT' && el.nodeName != 'TEXTAREA' && el.nodeName != 'SELECT' && el.children.length == 0,
           data = {};
@@ -20,8 +21,8 @@ var captureEditTd = (function ($) {
           el.blur();
         } else if (nl) {
           // save
-          data ={
-            element:el
+          data = {
+            element : el
           }
           // TODO when ready we could send an ajax request to update the field
           // $.ajax({
@@ -39,12 +40,14 @@ var captureEditTd = (function ($) {
 
     function returnData(new_data) {
       if(table === "#csv-table") {
-         var ovd_rate = $(new_data.element).html();
-         var st_rate = $(new_data.element).prevAll().eq(1).html();
-         var minus = st_rate - ovd_rate
-         var percent = ( (st_rate - ovd_rate) / st_rate) * 100;
-         $(new_data.element).next('td').html(percent.toFixed(2)+ "%");
-      }
+        if($(new_data.element).html() != NaN && $.isNumeric($(new_data.element).html()) && $(new_data.element).html()) {
+             var ovd_rate = $(new_data.element).html();
+             var st_rate = $(new_data.element).prevAll().eq(1).html();
+             var minus = st_rate - ovd_rate
+             var percent = ( (st_rate - ovd_rate) / st_rate) * 100;
+             $(new_data.element).next('td').html(percent.toFixed(2)+ "%");
+          }
+        }
     }
   }
 
--- src/js/components/customBillTable.js
+++ src/js/components/customBillTable.js
@@ -11,27 +11,29 @@ var loadCustomBillSheet = (function ($) {
         new_table = $('#new-table');
 
     function uploadCSV(data) {
-      var rows = data.split(/\n/),
-          titles = '';
-      //get titles from the Excel sheet
-      for (var i = rows.length - 1; i >= 1; i--) {
-        titles = rows[0].split(/","/g);
+      var newlines = /\r|\n/.exec(data);
+      if(newlines) {
+        var rows = data.split(/\n/),
+            titles = '';
+        //get titles from the Excel sheet
+        for (var i = rows.length - 1; i >= 1; i--) {
+          if(rows[0].indexOf(',') != -1) {
+            titles = rows[0].split(/","/g);
 
-        if($.trim(titles[0]) === "Title") {
-          titles[0] =  titles[0].replace(/"/g, "," );
-          titles[titles.length-1] = titles[titles.length-1].replace( /"/g, "," );
+            if($.trim(titles[0]) === "Title") {
+              titles[0] =  titles[0].replace(/"/g, "," );
+              titles[titles.length-1] = titles[titles.length-1].replace( /"/g, "," );
 
-          } else {
-            titles = ['Title', 'Grade', 'Rate','Currency', 'Upload / Override', 'Discount'];
+            } else {
+              titles = ['Title', 'Grade', 'Rate','Currency', 'Upload / Override', 'Discount'];
+            }
           }
         }
 
         rows = rows.map(function(row, index) {
-          // var matched = row.match(/"[^"]+"/g).split();
           var columns = row.split(",");
           columns[0] =  columns[0].replace(/"/g, "");
           columns[columns.length-1] = columns[columns.length-1].replace(/"/g, " ");
-
           return columns;
         });
 
@@ -39,35 +41,38 @@ var loadCustomBillSheet = (function ($) {
           rows.shift();
         }
 
-      csv_table.dataTable({
-        dom:'<tip>',
-        data: rows,
-        select: {
-          items: 'cells',
-          info: false
-        },
-        searching: false,
-        paging: false,
-        length: false,
-        bDestroy: true,
-        // order: [[ 0, 'desc' ]],
-        select: true,
-        columns: [
-          { title: titles[0] },
-          { title: titles[1] },
-          { title: titles[2] },
-          { title: titles[3] },
-          { title: titles[4] },
-          { title: titles[5] },
-        ],
-         "fnRowCallback": function (nRow, aData, iDisplayIndex, iDisplayIndexFull) {
-            $("td:nth-child(n+3):not(:last-child)", nRow).prop('contenteditable', true).addClass("contenteditable");
-             $(nRow).removeClass('odd even');
-            $('#new-table_wrapper').addClass('hide');
-            $('.custom-bill-sheet #add-row').addClass('hide');
-            this.removeClass('hide');
-         },
-      });
+        csv_table.dataTable({
+          dom:'<tip>',
+          data: rows,
+          select: {
+            items: 'cells',
+            info: false
+          },
+          searching: false,
+          paging: false,
+          length: false,
+          // order: [[ 0, 'desc' ]],
+          select: true,
+          columns: [
+            { title: titles[0] },
+            { title: titles[1] },
+            { title: titles[2] },
+            { title: titles[3] },
+            { title: titles[4] },
+            { title: titles[5] },
+          ],
+           "fnRowCallback": function (nRow, aData, iDisplayIndex, iDisplayIndexFull) {
+              $("td:nth-child(n+3):not(:last-child)", nRow).prop('contenteditable', true).addClass("contenteditable");
+               $(nRow).removeClass('odd even');
+               $("td:nth-child(3)", nRow).addClass('rate');
+              $('#new-table_wrapper').addClass('hide');
+              $('.custom-bill-sheet #add-row').addClass('hide');
+              this.removeClass('hide');
+           },
+          bDestroy: true,
+        });
+      }
+
     }
 
     $('.arrowpointer').on('click', function() {
@@ -80,29 +85,29 @@ var loadCustomBillSheet = (function ($) {
 
     // Upload CSV into a table.
     function uploadTable() {
-      $("#uploadTable").on('click', function(opt_startByte, opt_stopByte, e) {
+      $("#uploadTable").on('click', function(event, opt_startByte, opt_stopByte) {
+        $("input[type=\"file\"]").trigger('click', function() {});
+        $("input[type=\"file\"]").on('change', function(evt) {
 
-          $("input[type=\"file\"]").trigger('click', function() {});
+          var files = evt.target.files,
+            file = files[0],
+            file_name = file.name,
+            start = parseInt(opt_startByte) || 0,
+            stop = parseInt(opt_stopByte) || file.size - 1,
+            reader = new FileReader();
 
-          $("input[type=\"file\"]").on('change', function(evt) {
-
-            var files = evt.target.files,
-              file = files[0],
-              file_name = file.name,
-              start = parseInt(opt_startByte) || 0,
-              stop = parseInt(opt_stopByte) || file.size - 1,
-              reader = new FileReader();
-
-            reader.onloadend = function(event) {
-              if (event.target.readyState == FileReader.DONE) { // DONE == 2
-                uploadCSV(event.target.result);
-              }
-            };
-              var blob = file.slice(start, stop + 1);
-                reader.readAsBinaryString(blob);
-              $('#bill-sheet-name').val(file_name.slice(0,-4));
-          });
+          reader.onloadend = function(event) {
+            if (event.target.readyState == FileReader.DONE) { // DONE == 2
+              uploadCSV(event.target.result);
+            }
+          };
+            var blob = file.slice(start, stop + 1);
+              reader.readAsBinaryString(blob);
+            $('#bill-sheet-name').val(file_name.slice(0,-4));
+        });
+        event.stopPropagation();
       });
+
     }
     uploadTable();
 
