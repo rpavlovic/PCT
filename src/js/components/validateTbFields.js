7dc0045d6944e17ddfb9163e0f896dd811d46b67
adulfan <Anna.Dulfan@ey.com>
2017-02-01 11:42:07 -0500

    removed test json files and added a new one

--- src/js/components/validateTbFields.js
+++ src/js/components/validateTbFields.js
@@ -47,31 +47,20 @@ function returnData(new_data, table) {
     active_modeling_tabs.removeClass('active');
     active_modeling_tabs.children('input').prop('checked', false);
     //activate Adjusted Resource Hdr when override is entered.
-    // if(isNum) {
-    //   if(RateOverride.length > 0) {
-    //       console.log("Number: " + RateOverride.length > 0 + "NUMEBER: " + isNum)
-    //       $(active_modeling_tabs[2]).addClass('active');
-    //       $(active_modeling_tabs[2]).children('input').prop('checked', true);
-    //   } else {
-    //     console.log("Number: " + RateOverride.length > 0 + "NUMEBER: " + isNum)
-    //     $(active_modeling_tabs[1]).addClass('active');
-    //     $(active_modeling_tabs[1]).children('input').prop('checked', true);
-    //   }
-    // }
     if(RateOverride.length > 0) {
       if(isNum) {
-        console.log("Number: " + RateOverride.length > 0 + "NUMEBER: " + isNum)
+     //   console.log("Number: " + RateOverride.length > 0 + "NUMEBER: " + isNum)
         $(active_modeling_tabs[2]).addClass('active');
         $(active_modeling_tabs[2]).children('input').prop('checked', true);
       } else {
         error();
-        console.log("Number: " + RateOverride.length > 0 + "NUMEBER: " + isNum)
+      //  console.log("Number: " + RateOverride.length > 0 + "NUMEBER: " + isNum)
         $(active_modeling_tabs[2]).addClass('active');
         $(active_modeling_tabs[2]).children('input').prop('checked', true);
       }
     }
     else {
-      console.log(RateOverride.length > 0)
+     // console.log(RateOverride.length > 0)
       error();
       $(active_modeling_tabs[1]).addClass('active');
       $(active_modeling_tabs[1]).children('input').prop('checked', true);
