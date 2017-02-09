145923b73c96815c3f313aa6c4dbd1dc32661fad
adulfan <Anna.Dulfan@ey.com>
2017-02-09 15:58:26 -0500

    calculations

--- src/js/components/resourceCalculations.js
+++ src/js/components/resourceCalculations.js
@@ -14,7 +14,8 @@ var resourceFormulas = (function ($) {
       months_hours : $(table + " tbody .month"),
       bill_rate : $(table + "tbody td-billrate"),
       avarage_rate : $("#modeling-table tbody #avg-rate_standard-resource"),
-      modeling_fees: $("#modeling-table tbody #total-fee_standard-resource")
+      modeling_fees: $("#modeling-table tbody #total-fee_standard-resource"),
+      adjusted_rate_fees: $("#modeling-table tbody #total-fee_standard-resource")
     };
     var REgex_num = /^[\$]?[0-9\.\,]+[\%]?/g;
     var REgex_dollar = /(\d)(?=(\d\d\d)+(?!\d))/g;
@@ -49,9 +50,13 @@ var resourceFormulas = (function ($) {
 
     if(sum_hours > 0 && !isNaN(sum_hours)) {
       $(table1.row).find(table1.total_hours).text(sum_hours.toFixed(2));
+    } else {
+      $(table1.row).find(table1.total_hours).text('');
     }
     if(sum_rate > 0) {
       $(table1.row).find(table1.total_fees).text('$' + sum_rate.toFixed(3).replace(REgex_dollar, "$1,"));
+    } else {
+      $(table1.row).find(table1.total_fees).text('');
     }
 
     //show total in the footer
@@ -60,20 +65,33 @@ var resourceFormulas = (function ($) {
     });
     if(sum_hours > 0) {
       $('tfoot td.total-hours').text(total_hours.toFixed(2));
+    } else {
+      $('tfoot td.total-hours').text('');
     }
 
     table1.total_fees.each(function() {
       total_rate_sum += Number($(this).text().replace(/[^0-9\.]/g,""));
     });
+
     if(sum_rate > 0) {
       var total_fees = "$" + total_rate_sum.toFixed(3).replace(REgex_dollar, "$1,");
       table1.modeling_fees.text(total_fees);
+      table1.adjusted_rate_fees.text(total_fees);
       $('tfoot td.total-fees').text(total_fees);
+    } else {
+      table1.modeling_fees.text('');
+      table1.adjusted_rate_fees.text('');
+      $('tfoot td.total-fees').text('');
+    }
+        //modeling table populate
+    if(total_rate_sum > 0 && total_hours > 0) {
+      var av_rate =  total_rate_sum/total_hours;
+      table1.avarage_rate.text("$" + av_rate.toFixed(3).replace(REgex_dollar, "$1,"));
+    } else {
+       table1.avarage_rate.text('');
     }
 
-    //modeling table populate
-    var av_rate =  total_rate_sum/total_hours;
-    table1.avarage_rate.text("$" + av_rate.toFixed(3).replace(REgex_dollar, "$1,"));
+
 
    // Contribution Margin (total_rate_sum - total cost) / total_rate_sum + '%';
 
