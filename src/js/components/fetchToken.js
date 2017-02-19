7c13fe5d5607b55816fc6a382c647408afdc549d
Richard Dinh <richard.dinh@ey.com>
2017-02-19 11:26:24 -0500

    refactored fetch token into Ajax page setup routine.

---
+++ src/js/components/fetchToken.js
@@ -0,0 +1,31 @@
+/**
+ * @module Generates a Unique ID
+ * @version
+ */
+function fetchToken() {
+  if (is_fiori()) {
+    $.ajax({
+      method: "GET",
+      url: "/sap/opu/odata/sap/ZUX_PCT_SRV/$metadata",
+      beforeSend: function (request) {
+        request.setRequestHeader("X-CSRF-Token", "Fetch");
+      }
+    }).then(function (data, status, xhr) {
+      var token = xhr.getResponseHeader("X-CSRF-Token");
+      console.log(xhr.getResponseHeader("X-CSRF-Token"));
+      $.ajaxSetup({
+        headers: {
+          'X-CSRF-TOKEN': token
+        }
+      });
+    });
+  }
+  else{
+    console.log('running in local dev, so setting fake token for testing.');
+    $.ajaxSetup({
+      headers: {
+        'X-CSRF-TOKEN': 'faketoken'
+      }
+    });
+  }
+}
