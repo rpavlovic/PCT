09f58a5e303ca369e79a63ea7c1dde70dbd4961f
Richard Dinh <richard.dinh@ey.com>
2017-02-20 15:05:24 -0500

    refactored some code

--- src/js/components/convertToDecimal.js
+++ src/js/components/convertToDecimal.js
@@ -1,7 +1,7 @@
 /**
- * @param amount - takes in a string dollar amount e.g. $1,000,123.00 and just returns 1000123.00
+ * @param amount - takes in a string dollar amount e.g. $1,000,123.00 and just returns 1000123.00, or takes 25.0% and returns 25.0
  * @returns {string}
  */
 function convertToDecimal(amount){
-  return amount.replace('$', '').replace(',', '').toString();
+  return amount.replace('$', '').replace(',', '').replace('%', '').toString();
 }
