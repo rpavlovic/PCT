6e6fc33d629a4bcc68bb399e686c9dadeea300bb
Richard Dinh <richard.dinh@ey.com>
2017-02-24 15:39:39 -0500

    delete project info page.

--- src/js/components/activeProjectsTable.js
+++ src/js/components/activeProjectsTable.js
@@ -1,14 +1,13 @@
-
 /**
-* @module Draw Data Table for Active Projects.
-* @version
-*/
+ * @module Draw Data Table for Active Projects.
+ * @version
+ */
 var activeTableFunction = (function ($) {
   'use strict';
 
-  $('#start-project').on('click', function(e){
+  $('#start-project').on('click', function (e) {
     e.preventDefault();
-    window.location.href = 'projectGeneral.htm?projID='+ get_project_id();
+    window.location.href = 'projectGeneral.htm?projID=' + get_project_id();
   });
 
   function initActiveTable() {
@@ -18,80 +17,85 @@ var activeTableFunction = (function ($) {
     var activeTable = table.DataTable({
       "sDom": '<"toolbar"><B><tip>',
       "searching": true,
-      "ajax" : {
-         "url": get_data_feed( feeds.projectList ),
-          "dataSrc": "d.results"
-       },
-      "bServerSide" : false,
+      "ajax": {
+        "url": get_data_feed(feeds.projectList),
+        "dataSrc": "d.results"
+      },
+      "bServerSide": false,
       "iDisplayLength": 10,
       "bAutoWidth": false,
       "stateSave": true,
-      "columnDefs": [ {
+      "columnDefs": [{
         "orderable": false,
         "targets": [5, 6]
-        }],
+      }],
       "aoColumns": [
-      {
-        "sTitle": 'Project Name',
-        "data":"Projname",
-        "render": function ( data, type, set, meta ) {
-          if(data) {
-            var output = '<a href="projectGeneral.htm?projID=' + set.Projid + '&projName=' + data + '" title="ProjectName">';
-            output += data;
-            output += '</a>';
-            return output;
+        {
+          "sTitle": 'Project Name',
+          "data": "Projname",
+          "render": function (data, type, set, meta) {
+            if (data) {
+              var output = '<a href="projectGeneral.htm?projID=' + set.Projid + '&projName=' + data + '" title="ProjectName">';
+              output += data;
+              output += '</a>';
+              return output;
+            }
+          }
+        },
+        {
+          "sTitle": 'Billing Office',
+          "data": "Office"
+        },
+        {
+          "title": 'Est.Date',
+          "data": "EstStDate",
+          "defaultContent": '',
+          "render": function (data, type) {
+            var str = data;
+            var num = parseInt(str.replace(/[^0-9]/g, ""));
+            var date = new Date(num);
+            if (type === 'display' || type === 'filter') {
+              var d = date;
+              return d.getDate() + '/' + (d.getMonth() + 1) + '/' + d.getFullYear();
+            }
+            // Otherwise the data type requested (`type`) is type detection or
+            // sorting data, for which we want to use the integer, so just return
+            // that, unaltered
+            return data;
+          }
+        },
+        {
+          "title": 'Duration',
+          "data": "Duration",
+          "defaultContent": ''
+        },
+        {
+          "title": 'Budget',
+          "data": " ",
+          "defaultContent": ''
+        },
+        {
+          "title": '<i class="fa fa-files-o"></i>',
+          "sClass": "center blue-bg",
+          "data": "Projid",
+          "defaultContent": '',
+          "render": function (data, type, set, meta) {
+            if (data) {
+              return '<a href="projectGeneral.htm?projID=' + data + '&projName=' + set.Projname + '" class=""><i class="fa fa-files-o"></i></a>';
+            }
+          },
+        },
+        {
+          "title": '<i class="fa fa-trash"></i>',
+          "sClass": "center blue-bg",
+          "data": "Projid",
+          "defaultContent": '<a href="" class="remove"><i class="fa fa-trash"></i></a>',
+          "render": function (data, type, set, meta) {
+            if (data) {
+              return '<a href="" data-projid="' + data + '" class="remove"><i class="fa fa-trash"></i></a>';
+            }
           }
         }
-      },
-      {
-        "sTitle": 'Billing Office',
-        "data":"Office"
-      },
-      {
-        "title": 'Est.Date',
-        "data":"EstStDate",
-        "defaultContent": '',
-        "render" : function(data, type) {
-          var str = data;
-          var num = parseInt(str.replace(/[^0-9]/g, ""));
-          var date = new Date(num);
-          if ( type === 'display' || type === 'filter' ) {
-           var d = date;
-           return d.getDate() +'/'+ (d.getMonth()+1) +'/'+ d.getFullYear();
-          }
-         // Otherwise the data type requested (`type`) is type detection or
-         // sorting data, for which we want to use the integer, so just return
-         // that, unaltered
-         return data;
-         }
-      },
-      {
-        "title": 'Duration',
-        "data": "Duration",
-        "defaultContent": ''
-      },
-      {
-        "title": 'Budget',
-        "data":" ",
-        "defaultContent": ''
-      },
-      {
-        "title" : '<i class="fa fa-files-o"></i>',
-        "sClass": "center blue-bg",
-        "data": "Projid",
-        "defaultContent": '',
-        "render": function ( data, type, set, meta ) {
-          if(data) {
-            return '<a href="projectGeneral.htm?projID=' + data + '&projName=' + set.Projname + '" class=""><i class="fa fa-files-o"></i></a>';
-          }
-        }
-      },
-      {
-        "title" : '<i class="fa fa-trash"></i>',
-        "sClass": "center blue-bg",
-        "data": null,
-        "defaultContent":'<a href="" class="remove"><i class="fa fa-trash"></i></a>'
-      }
       ],
       "bFilter": true,
       "select": true,
@@ -109,29 +113,29 @@ var activeTableFunction = (function ($) {
       "fnInitComplete": function (nRow) {
         this.api().columns().every(function (index, fn) {
           var column = this;
-          var field_wrapper =$('<div class="field-wrapper"></div>');
-          var label = $('<label for="search_'+index+'" class="show">Filter</label>');
-          if(index < 5) {
-            var select = $('<select name="search_'+index+'" selected><option value=""></option></select>')
-            .appendTo( "div.toolbar")
+          var field_wrapper = $('<div class="field-wrapper"></div>');
+          var label = $('<label for="search_' + index + '" class="show">Filter</label>');
+          if (index < 5) {
+            var select = $('<select name="search_' + index + '" selected><option value=""></option></select>')
+              .appendTo("div.toolbar")
               .on('change', function () {
                 var val = $.fn.dataTable.util.escapeRegex(
                   $(this).val()
                 );
-                column.search( val ? '^' + val + '$' : '', true, false ).draw();
+                column.search(val ? '^' + val + '$' : '', true, false).draw();
               });
 
-              select.wrap(field_wrapper);
-              label.insertBefore(select);
-              //create filters
-              column.data().unique().sort().each( function ( d ) {
-                if(typeof d === 'string' && d.indexOf('/Date') != -1) {
-                  var str = d;
-                  var num = parseInt(str.replace(/[^0-9]/g, ""));
-                  d = new Date(num);
-                  d = d.getDate() +'/'+ (d.getMonth()+1) +'/'+ d.getFullYear();
-                }
-                select.append( '<option value="'+ d +'">' + d + '</option>' );
+            select.wrap(field_wrapper);
+            label.insertBefore(select);
+            //create filters
+            column.data().unique().sort().each(function (d) {
+              if (typeof d === 'string' && d.indexOf('/Date') != -1) {
+                var str = d;
+                var num = parseInt(str.replace(/[^0-9]/g, ""));
+                d = new Date(num);
+                d = d.getDate() + '/' + (d.getMonth() + 1) + '/' + d.getFullYear();
+              }
+              select.append('<option value="' + d + '">' + d + '</option>');
             });
           }
         });
@@ -144,16 +148,30 @@ var activeTableFunction = (function ($) {
     //   console.log( json.d.results );
     // });
 
-    $('.search-table').on( 'keyup change search', function (e) {
-       activeTable.search( this.value ).draw();
+    $('.search-table').on('keyup change search', function (e) {
+      activeTable.search(this.value).draw();
     });
     // $( '.buttons-page-length' ).insertAfter('#active-projects_wrapperarchived-projects_wrapper').wrap("<div class=\"dt-buttons\" />").addClass('float-right');
-    $('#active-projects tbody').on( 'click', '.remove', function (e) {
+    $('#active-projects tbody').on('click', '.remove', function (e) {
       e.preventDefault();
-      activeTable.row( $(this).parents('tr') ).remove().draw();
+
+      console.log($(this));
+      console.log($(this).data('projid'));
+      var projectId = $(this).data('projid');
+      $.ajax({
+        url: "/sap/opu/odata/sap/ZUX_PCT_SRV/ProjectInfoCollection('" +  projectId + "')",
+        type: 'DELETE',
+        success: function (result) {
+          // Do something with the result
+          activeTable.row($(this).parents('tr')).remove().draw();
+        }
+      });
+
+
     });
   }
+
   return {
-    initActiveTable:initActiveTable
+    initActiveTable: initActiveTable
   };
 })($);
