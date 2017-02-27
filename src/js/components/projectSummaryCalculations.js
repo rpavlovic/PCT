16077f4050c3297d08e69f32086cfcd95e4b3616
Richard Dinh <richard.dinh@ey.com>
2017-02-27 13:50:40 -0500

    oops

---
+++ src/js/components/projectSummaryCalculations.js
@@ -0,0 +1,44 @@
+/**
+ * @module Calulate summary info for each project
+ * @version
+ */
+
+var projectSummaryCalculations = (function ($) {
+  'use strict';
+
+  function calculateBudget(projectId) {
+    var pExpenses = new Promise(function (resolve, reject) {
+      $.getJSON(get_data_feed(feeds.projectExpenses, projectId), function (projectDeliverables) {
+        resolve(projectDeliverables.d.results);
+      });
+    });
+    var pResources = new Promise(function (resolve, reject) {
+      $.getJSON(get_data_feed(feeds.projectResources, projectId), function (rateCard) {
+        resolve(rateCard.d.results);
+      });
+    });
+
+    return Promise.all([pExpenses, pResources]).then(function (values) {
+      var expenses = values[0];
+      var resources = values[1];
+      console.log(expenses);
+      console.log(resources);
+
+      var expenseTotal = expenses.reduce(function(acc, val){
+        return parseFloat(acc) + parseFloat(val.Amount);
+      }, 0);
+
+      var resourceTotalFee = resources.reduce(function(acc, val){
+        return parseFloat(acc) + parseFloat(val.TotalFee);
+      }, 0);
+      console.log(expenseTotal);
+
+      return expenseTotal + resourceTotalFee;
+    });
+
+  }
+
+  return {
+    calculateBudget: calculateBudget
+  };
+})($);
