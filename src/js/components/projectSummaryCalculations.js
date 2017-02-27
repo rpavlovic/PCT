f922e1727cc6ea21c4fc97fd5f78d655a0ef421f
Richard Dinh <richard.dinh@ey.com>
2017-02-27 14:46:21 -0500

    saving off some work

--- src/js/components/projectSummaryCalculations.js
+++ src/js/components/projectSummaryCalculations.js
@@ -34,7 +34,41 @@ var projectSummaryCalculations = (function ($) {
 
   }
 
+  function calculateFinancialSummary(projectId) {
+    var pExpenses = new Promise(function (resolve, reject) {
+      $.getJSON(get_data_feed(feeds.projectExpenses, projectId), function (projectDeliverables) {
+        resolve(projectDeliverables.d.results);
+      });
+    });
+    var pResources = new Promise(function (resolve, reject) {
+      $.getJSON(get_data_feed(feeds.projectResources, projectId), function (rateCard) {
+        resolve(rateCard.d.results);
+      });
+    });
+
+    return Promise.all([pExpenses, pResources]).then(function (values) {
+      var expenses = values[0];
+      var resources = values[1];
+      var expenseTotal = expenses.reduce(function(acc, val){
+        return parseFloat(acc) + parseFloat(val.Amount);
+      }, 0);
+
+      var resourceTotalFee = resources.reduce(function(acc, val){
+        return parseFloat(acc) + parseFloat(val.TotalFee);
+      }, 0);
+
+      var summary = {
+        budget: expenseTotal + resourceTotalFee,
+        expenses: expenseTotal
+      };
+
+      return summary;
+    });
+  }
+
+
   return {
-    calculateBudget: calculateBudget
+    calculateBudget: calculateBudget,
+    calculateFinancialSummary: calculateFinancialSummary
   };
 })($);
