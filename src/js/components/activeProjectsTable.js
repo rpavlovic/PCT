d7f55397ce3d49f1927fcfb78ccee9453517470b
Richard Dinh <richard.dinh@ey.com>
2017-02-27 12:25:51 -0500

    more efficient way of doing things.

--- src/js/components/activeProjectsTable.js
+++ src/js/components/activeProjectsTable.js
@@ -11,163 +11,173 @@ var activeTableFunction = (function ($) {
   });
 
   function initActiveTable() {
-
     var table = $('#active-projects');
-
-    var activeTable = table.DataTable({
-      "sDom": '<"toolbar"><B><tip>',
-      "searching": true,
-      "ajax": {
-        "url": get_data_feed(feeds.projectList),
-        "dataSrc": "d.results"
-      },
-      "bServerSide": false,
-      "iDisplayLength": 10,
-      "bAutoWidth": false,
-      "stateSave": true,
-      "columnDefs": [{
-        "orderable": false,
-        "targets": [5, 6]
-      }],
-      "aoColumns": [
-        {
-          "sTitle": 'Project Name',
-          "data": "Projname",
-          "render": function (data, type, set, meta) {
-            if (data) {
-              var output = '<a href="projectGeneral.htm?projID=' + set.Projid + '&projName=' + data + '" title="ProjectName">';
-              output += data;
-              output += '</a>';
-              return output;
-            }
-          }
-        },
-        {
-          "sTitle": 'Billing Office',
-          "data": "Office"
-        },
-        {
-          "title": 'Est.Date',
-          "data": "EstStDate",
-          "defaultContent": '',
-          "render": function (data, type) {
-            var str = data;
-            var num = parseInt(str.replace(/[^0-9]/g, ""));
-            var date = new Date(num);
-            if (type === 'display' || type === 'filter') {
-              var d = date;
-              return d.getDate() + '/' + (d.getMonth() + 1) + '/' + d.getFullYear();
-            }
-            // Otherwise the data type requested (`type`) is type detection or
-            // sorting data, for which we want to use the integer, so just return
-            // that, unaltered
-            return data;
-          }
-        },
-        {
-          "title": 'Duration',
-          "data": "Duration",
-          "defaultContent": ''
-        },
-        {
-          "title": 'Budget',
-          "data": " ",
-          "defaultContent": ''
-        },
-        {
-          "title": '<i class="fa fa-files-o"></i>',
-          "sClass": "center blue-bg",
-          "data": "Projid",
-          "defaultContent": '',
-          "render": function (data, type, set, meta) {
-            if (data) {
-              return '<a href="projectGeneral.htm?projID=' + data + '&projName=' + set.Projname + '" class=""><i class="fa fa-files-o"></i></a>';
+    var p2 = new Promise(function (resolve, reject) {
+      $.getJSON(get_data_feed(feeds.projectList), function (projects) {
+        resolve(projects.d.results);
+      }).fail(function () {
+        // not found, but lets fix this and return empty set
+        console.log('no projects found.... returning empty set');
+        resolve([]);
+      });
+    }).then(function (projects) {
+      var activeTable = table.DataTable({
+        "sDom": '<"toolbar"><B><tip>',
+        "searching": true,
+        "data": projects,
+        "bServerSide": false,
+        "iDisplayLength": 10,
+        "bAutoWidth": false,
+        "stateSave": true,
+        "columnDefs": [{
+          "orderable": false,
+          "targets": [5, 6]
+        }],
+        "aoColumns": [
+          {
+            "sTitle": 'Project Name',
+            "data": "Projname",
+            "render": function (data, type, set, meta) {
+              if (data) {
+                var output = '<a href="projectGeneral.htm?projID=' + set.Projid + '&projName=' + data + '" title="ProjectName">';
+                output += data;
+                output += '</a>';
+                return output;
+              }
             }
           },
-        },
-        {
-          "title": '<i class="fa fa-trash"></i>',
-          "sClass": "center blue-bg",
-          "data": "Projid",
-          "defaultContent": '<a href="" class="remove"><i class="fa fa-trash"></i></a>',
-          "render": function (data, type, set, meta) {
-            if (data) {
-              return '<a href="" data-projid="' + data + '" class="remove"><i class="fa fa-trash"></i></a>';
+          {
+            "sTitle": 'Billing Office',
+            "data": "Office"
+          },
+          {
+            "title": 'Est.Date',
+            "data": "EstStDate",
+            "defaultContent": '',
+            "render": function (data, type) {
+              var str = data;
+              var num = parseInt(str.replace(/[^0-9]/g, ""));
+              var date = new Date(num);
+              if (type === 'display' || type === 'filter') {
+                var d = date;
+                return d.getDate() + '/' + (d.getMonth() + 1) + '/' + d.getFullYear();
+              }
+              // Otherwise the data type requested (`type`) is type detection or
+              // sorting data, for which we want to use the integer, so just return
+              // that, unaltered
+              return data;
+            }
+          },
+          {
+            "title": 'Duration',
+            "data": "Duration",
+            "defaultContent": ''
+          },
+          {
+            "title": 'Budget',
+            "data": 'Projid',
+            "defaultContent": '',
+            "render": function (data, type, set, meta) {
+              if (data) {
+                return data;
+               // return '<a href="projectGeneral.htm?projID=' + data + '&projName=' + set.Projname + '" class=""><i class="fa fa-files-o"></i></a>';
+              }
+            }
+
+          },
+          {
+            "title": '<i class="fa fa-files-o"></i>',
+            "sClass": "center blue-bg",
+            "data": "Projid",
+            "defaultContent": '',
+            "render": function (data, type, set, meta) {
+              if (data) {
+                return '<a href="projectGeneral.htm?projID=' + data + '&projName=' + set.Projname + '" class=""><i class="fa fa-files-o"></i></a>';
+              }
+            }
+          },
+          {
+            "title": '<i class="fa fa-trash"></i>',
+            "sClass": "center blue-bg",
+            "data": "Projid",
+            "defaultContent": '<a href="" class="remove"><i class="fa fa-trash"></i></a>',
+            "render": function (data, type, set, meta) {
+              if (data) {
+                return '<a href="" data-projid="' + data + '" class="remove"><i class="fa fa-trash"></i></a>';
+              }
             }
           }
-        }
-      ],
-      "bFilter": true,
-      "select": true,
-      "buttons": [
-        'copy', 'csv', 'excel',
-        {
-          "extend": 'pdf',
-          "download": 'open'
-        }
-      ],
-      "fnRowCallback": function (nRow, aData, iDisplayIndex, iDisplayIndexFull) {
-        $(nRow).removeClass('odd even');
-      },
-      //when json is loaded add the filters to the toolbar div.
-      "fnInitComplete": function (nRow) {
-        this.api().columns().every(function (index, fn) {
-          var column = this;
-          var field_wrapper = $('<div class="field-wrapper"></div>');
-          var label = $('<label for="search_' + index + '" class="show">Filter</label>');
-          if (index < 5) {
-            var select = $('<select name="search_' + index + '" selected><option value=""></option></select>')
-              .appendTo("div.toolbar")
-              .on('change', function () {
-                var val = $.fn.dataTable.util.escapeRegex(
-                  $(this).val()
-                );
-                column.search(val ? '^' + val + '$' : '', true, false).draw();
+        ],
+        "bFilter": true,
+        "select": true,
+        "buttons": [
+          'copy', 'csv', 'excel',
+          {
+            "extend": 'pdf',
+            "download": 'open'
+          }
+        ],
+        "fnRowCallback": function (nRow, aData, iDisplayIndex, iDisplayIndexFull) {
+          $(nRow).removeClass('odd even');
+        },
+        //when json is loaded add the filters to the toolbar div.
+        "fnInitComplete": function (nRow) {
+          this.api().columns().every(function (index, fn) {
+            var column = this;
+            var field_wrapper = $('<div class="field-wrapper"></div>');
+            var label = $('<label for="search_' + index + '" class="show">Filter</label>');
+            if (index < 5) {
+              var select = $('<select name="search_' + index + '" selected><option value=""></option></select>')
+                .appendTo("div.toolbar")
+                .on('change', function () {
+                  var val = $.fn.dataTable.util.escapeRegex(
+                    $(this).val()
+                  );
+                  column.search(val ? '^' + val + '$' : '', true, false).draw();
+                });
+
+              select.wrap(field_wrapper);
+              label.insertBefore(select);
+              //create filters
+              column.data().unique().sort().each(function (d) {
+                if (typeof d === 'string' && d.indexOf('/Date') != -1) {
+                  var str = d;
+                  var num = parseInt(str.replace(/[^0-9]/g, ""));
+                  d = new Date(num);
+                  d = d.getDate() + '/' + (d.getMonth() + 1) + '/' + d.getFullYear();
+                }
+                select.append('<option value="' + d + '">' + d + '</option>');
               });
+            }
+          });
+          $('.toolbar').hide();
+        },
+        "bDestroy": true
+      });
 
-            select.wrap(field_wrapper);
-            label.insertBefore(select);
-            //create filters
-            column.data().unique().sort().each(function (d) {
-              if (typeof d === 'string' && d.indexOf('/Date') != -1) {
-                var str = d;
-                var num = parseInt(str.replace(/[^0-9]/g, ""));
-                d = new Date(num);
-                d = d.getDate() + '/' + (d.getMonth() + 1) + '/' + d.getFullYear();
-              }
-              select.append('<option value="' + d + '">' + d + '</option>');
-            });
-          }
-        });
-        $('.toolbar').hide();
-      },
-      "bDestroy": true,
-    });
-    // activeTable.api().on( 'xhr', function () {
-    //   var json = activeTable.api().ajax.json();
-    //   console.log( json.d.results );
-    // });
+      $('.search-table').on('keyup change search', function (e) {
+        activeTable.search(this.value).draw();
+      });
 
-    $('.search-table').on('keyup change search', function (e) {
-      activeTable.search(this.value).draw();
-    });
-    // $( '.buttons-page-length' ).insertAfter('#active-projects_wrapperarchived-projects_wrapper').wrap("<div class=\"dt-buttons\" />").addClass('float-right');
-    $('#active-projects tbody').on('click', '.remove', function (e) {
-      e.preventDefault();
-      //console.log($(this));
-      //console.log($(this).data('projid'));
-      var r = confirm("Are you sure you want to delete this project?");
-      if (r === true) {
-        var projectId = $(this).data('projid');
-        $.ajax({
-          url: "/sap/opu/odata/sap/ZUX_PCT_SRV/ProjectInfoCollection('" + projectId + "')",
-          type: 'DELETE',
-          success: function (result) {
-            // Do something with the result
-            activeTable.row($(this).parents('tr')).remove().draw();
-          }
-        });
-      }
+      // $( '.buttons-page-length' ).insertAfter('#active-projects_wrapperarchived-projects_wrapper').wrap("<div class=\"dt-buttons\" />").addClass('float-right');
+      $('#active-projects tbody').on('click', '.remove', function (e) {
+        e.preventDefault();
+        //console.log($(this));
+        //console.log($(this).data('projid'));
+        var r = confirm("Are you sure you want to delete this project?");
+        var currentRow = $(this);
+        if (r === true) {
+          var projectId = $(this).data('projid');
+          $.ajax({
+            url: "/sap/opu/odata/sap/ZUX_PCT_SRV/ProjectInfoCollection('" + projectId + "')",
+            type: 'DELETE',
+            success: function (result) {
+              // Do something with the result
+              activeTable.row(currentRow.parents('tr')).remove().draw();
+            }
+          });
+        }
+      });
     });
   }
 
