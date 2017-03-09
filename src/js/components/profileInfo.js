9a56f6f25e98186fed6d52d571bebb0ef0106f91
adulfan <anna.dulfan@ey.com>
2017-03-08 21:10:22 -0500

    card names to the profile page

--- src/js/components/profileInfo.js
+++ src/js/components/profileInfo.js
@@ -14,19 +14,47 @@ var profileInfo = (function ($) {
     street = profile_form.find('.street'),
     city = profile_form.find('.city');
 
+    var OfficeId;
+
   function initProfileInfo(feeds) {
 
     $.getJSON(get_data_feed(feeds.employee), function (employees) {
-      var val = employees.d.results[0];
-      name.text(val.EmployeeFullName);
-      email.attr("href", "mailto:" + val.EmployeeEmail).text(val.EmployeeEmail);
-      phone.attr("href", "tel:" + val.OfficeTelnumber).text(val.OfficeTelnumber);
-      mobile.attr("href", "tel:" + val.EmployeePhoneNum).text(val.EmployeePhoneNum);
-      office.text(val.OfficeName).css('font-weight', 'bold');
-      street.text(val.OfficeHousenum + " " + val.OfficeStreet + ", " + val.OfficeFloor);
-      city.text(val.OfficeCity + ", " + val.OfficeRegion + ", " + val.OfficePostalcode);
+        var val = employees.d.results[0];
+        name.text(val.EmployeeFullName);
+        email.attr("href", "mailto:" + val.EmployeeEmail).text(val.EmployeeEmail);
+        phone.attr("href", "tel:" + val.OfficeTelnumber).text(val.OfficeTelnumber);
+        mobile.attr("href", "tel:" + val.EmployeePhoneNum).text(val.EmployeePhoneNum);
+        office.text(val.OfficeName).css('font-weight', 'bold');
+        street.text(val.OfficeHousenum + " " + val.OfficeStreet + ", " + val.OfficeFloor);
+        city.text(val.OfficeCity + ", " + val.OfficeRegion + ", " + val.OfficePostalcode);
+      });
+
+    $.getJSON(get_data_feed(feeds.billSheet), function (cards) {
+      $(cards).map(function(key, val) {
+        var cardName = val.d.results.filter(function( value ) {
+          console.log(value.BillsheetName);
+          return value.BillsheetName;
+        });
+
+      });
     });
+
+    var p2 = new Promise(function (resolve, reject) {
+      $.getJSON(get_data_feed(feeds.billSheet), function (cards) {
+        resolve(cards.d.results);
+      }).fail(function () {
+        // not found, but lets fix this and return empty set
+        console.log('no offices found.... returning empty set');
+        resolve([]);
+      });
+    });
+
+    console.log(p2)
+
+
+
   }
+
   return {
     initProfileInfo: initProfileInfo
   };
