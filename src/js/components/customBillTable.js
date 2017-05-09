705980d79c0808d2bd2649eb94d117be2fb0c10b
adulfan <Anna.Dulfan@ey.com>
2017-05-09 13:22:43 -0400

    class sort fix

--- src/js/components/customBillTable.js
+++ src/js/components/customBillTable.js
@@ -122,6 +122,7 @@ var loadCustomBillSheet = (function ($) {
           {
             "name": "Class",
             "data": "Class",
+            "title":"Class",
             "sType": "rclass",
             "defaultContent": ''
           },
@@ -173,7 +174,7 @@ var loadCustomBillSheet = (function ($) {
         searching: false,
         paging: false,
         length: false,
-        order: [[0, 'asc']],
+        order: [[2  , 'desc']],
         columns: columns,
         "fnRowCallback": function (nRow, aData, iDisplayIndex, iDisplayIndexFull) {
           $(nRow).removeClass('odd even');
